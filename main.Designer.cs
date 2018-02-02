cea88e06a447bf130e7df9d0aac16ab3e424ef2e
RuMengRen <xizirumeng@gmail.com>
2018-02-02 19:03:41 +0800

    解决换行符无法粘贴问题

--- main.Designer.cs
+++ main.Designer.cs
@@ -52,6 +52,7 @@
             this.textBox.Name = "textBox";
             this.textBox.Size = new System.Drawing.Size(314, 23);
             this.textBox.TabIndex = 0;
+            this.textBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyDown);
             this.textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
             this.textBox.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox_KeyUp);
             // 
