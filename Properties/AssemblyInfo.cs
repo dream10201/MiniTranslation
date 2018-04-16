505f865b61aaaccdd7ae040b7bc61ebc027a366d
RuMengRen <xizirumeng@gmail.com>
2018-04-16 19:01:27 +0800

    rename

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("MinTranslation")]
+[assembly: AssemblyTitle("MiniTranslation")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("www.xizirumeng.com")]
-[assembly: AssemblyProduct("MinTranslation")]
+[assembly: AssemblyCompany("bidd.net")]
+[assembly: AssemblyProduct("MiniTranslation")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
