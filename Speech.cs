50b7c5f8c74e640b31dbf3e381176d37dbe492da
RuMengRen <xizirumeng@gmail.com>
2018-04-16 19:59:29 +0800

    ...

---
+++ Speech.cs
@@ -0,0 +1,24 @@
+﻿using SpeechLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MiniTranslation
+{
+    class Speech
+    {
+        private SpVoice spVoice;
+        public Speech() {
+            spVoice = new SpVoice();
+            spVoice.Rate = 0; //语速,[-10,10]
+            spVoice.Volume = 100; //音量,[0,100]
+            //spVoice.Voice = spVoice.GetVoices().Item(0); //语音库
+            //voice.Speak(resultText);
+        }
+        public void Voice(String text) {
+            spVoice.Speak(text, SpeechVoiceSpeakFlags.SVSFlagsAsync | SpeechVoiceSpeakFlags.SVSFPurgeBeforeSpeak);
+        }
+
+    }
+}
