21e51620392c5073d24e17841dd45d96e94904df
dream10201 <x1349600266>
2019-07-04 10:06:35 +0800

    去除翻译结果中的id

--- resource/Constant.cs
+++ resource/Constant.cs
@@ -12,7 +12,8 @@ namespace MiniTranslation.resource
         public const String TranslateURL = "https://translate.google.cn/translate_a/single?client=gtx&dt=t&sl=";
         public const int WM_HOTKEY = 0x312; //窗口消息-热键  
         public const int WM_CREATE = 0x1; //窗口消息-创建  
-        public const int WM_DESTROY = 0x2; //窗口消息-销毁  
+        public const int WM_DESTROY = 0x2; //窗口消息-销毁
+        public const int WM_QUERYENDSESSION = 0x0011;
         public const int HOTKeyID = 0x3572; //热键ID
     }
 }
