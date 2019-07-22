5243d5a600775fb571d416cd8144a9e059562489
dream10201 <dream10201@qq.com>
2019-07-22 15:57:38 +0800

    可能解决了偶尔乱码问题

--- resource/Constant.cs
+++ resource/Constant.cs
@@ -8,8 +8,8 @@ namespace MiniTranslation.resource
 {
     class Constant
 {
-
-        public const String TranslateURL = "https://translate.google.cn/translate_a/single?ie=UTF-8&client=gtx&otf=1&ssel=0&tsel=0&kc=1&dt=t&hl=zh-CN&sl=";
+        //https://translate.google.cn/translate_a/single?ie=UTF-8&oe=UTF-8&client=gtx&otf=1&ssel=0&tsel=0&kc=1&dt=t&hl=en&sl=
+        public const String TranslateURL = "https://translate.google.cn/translate_a/single?ie=UTF-8&oe=UTF-8&tk=&client=gtx&dt=t&ssel=3&tsel=0&kc=1&otf=1&hl=zh-CN&sl=";
         public const int WM_HOTKEY = 0x312; //窗口消息-热键  
         public const int WM_CREATE = 0x1; //窗口消息-创建  
         public const int WM_DESTROY = 0x2; //窗口消息-销毁
