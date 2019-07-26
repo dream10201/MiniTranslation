434cba018941606c683127a30a83d2df5965bf40
dream10201 <dream10201@qq.com>
2019-07-26 11:38:39 +0800

    解决网络不畅时出现闪退

--- util/HttpUtils.cs
+++ util/HttpUtils.cs
@@ -11,12 +11,20 @@ namespace MiniTranslation.util
     class HttpUtils
     {
         public static string Get(string url) {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            StreamReader myreader = new StreamReader(request.GetResponse().GetResponseStream(), Encoding.UTF8);
-            string responseText = myreader.ReadToEnd();
-            myreader.Dispose();
-            myreader.Close();
-            return responseText;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                StreamReader myreader = new StreamReader(request.GetResponse().GetResponseStream(), Encoding.UTF8);
+                string responseText = myreader.ReadToEnd();
+                myreader.Dispose();
+                myreader.Close();
+                return responseText;
+            }
+            catch (Exception)
+            {
+
+            }
+            return "";
         }
     }
 }
